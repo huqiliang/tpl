fdee8232a181e1bc754277f06deaf5132fe700d1
胡奇良 <hql@ipms.cn>
2022-03-18 15:16:12 +0800

    feat:init project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import iView from "view-design"; // step 1:import view-design
+import "view-design/dist/styles/iview.css";
+import proIview from "pro_iview"; // step 1:import pro_iview
+import "pro_iview/lib/pro-iview.css";
+import http from "./libs/http";
+
+Vue.use(iView); // step 3:use iView
+Vue.use(proIview, {
+  http,
+}); // step 4:use proIview
 
 Vue.config.productionTip = false;
 
