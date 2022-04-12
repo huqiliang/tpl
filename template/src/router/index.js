7da34dcba92d141aebcb875ed595965e810b26f7
胡奇良 <hql@ipms.cn>
2022-04-12 13:45:25 +0800

    feat:增加链接token判断 写入缓存

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -2,11 +2,23 @@ import Vue from "vue";
 import Router from "vue-router";
 import autoRoutes from "vue-auto-routing";
 import myRoutes from "./routes";
+import _ from "lodash";
 
 Vue.use(Router);
+
 export const routes = [...autoRoutes, ...myRoutes];
 
-export default new Router({
+const router = new Router({
   base: process.env.BASE_URL,
   routes
 });
+
+router.beforeEach(async (to, from, next) => {
+  const { token } = to.query;
+  if (!_.isEmpty(token)) {
+    localStorage.setItem("token", token);
+  }
+  next();
+});
+
+export default router;
